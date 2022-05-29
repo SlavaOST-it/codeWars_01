536a7a649c31f008ca39b454ee5c851b09570e35
SlavaOST-it <slavaost-it@mail.ru>
2022-05-29 16:52:18 +0300

    добавлены решения задач 2, 3

--- 01.js
+++ 01.js
@@ -1,4 +1,42 @@
+// task 1
 function helloWorld() {
     let str = "Hello World!";
   console.log(str);
-}
+};
+
+
+// task 2
+function makeNegative(num) {
+  if (num > 0) {
+    return (num * (-1));
+  } else if (num === 0) {
+    return (0);
+  } else if (num < 0) {
+    return (num);
+  }
+};
+
+
+// task 3
+function week(day) {
+    if (day == 1) {
+        alert("Понедельник");
+    } else if (day == 2) {
+        alert("Вторник");
+    } else if (day == 3) {
+        alert("Среда");
+    } else if (day == 4) {
+        alert("Четверг");
+    } else if (day == 5) {
+        alert("Пятница");
+    } else if (day == 6) {
+        alert("Суббота");
+    } else if (day == 7) {
+        alert("Воскресенье");
+    }
+    
+};
+
+
+
+
