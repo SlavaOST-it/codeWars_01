30b9e4e021747dc69ad86c2e828b47dd3c94da53
SlavaOST-it <slavaost-it@mail.ru>
2022-05-30 10:35:29 +0300

    task 8

--- 01.js
+++ 01.js
@@ -68,3 +68,14 @@ function bmi(weight, height) {
   }   
 };
 
+
+// task 8
+function check(a, x) {
+  for (var i = 0; i < a.length; i++){
+    if (a[i] === x) {
+      return true;
+    }
+  }
+  return false;
+};
+
