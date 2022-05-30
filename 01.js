244a9a47f226bc46d93f8008454ec9f28c487b14
SlavaOST-it <slavaost-it@mail.ru>
2022-05-30 10:05:08 +0300

    task 7

--- 01.js
+++ 01.js
@@ -51,4 +51,20 @@ function multiply(a, b){
 // task 6
 function makeUpperCase(str) {
   return str.toUpperCase();
-}
+};
+
+
+// task 7
+function bmi(weight, height) {
+  let i = weight / (height * height);
+  if (i <= 18.5) {
+    return("Underweight");
+  } else if(i <= 25.0){
+    return("Normal");
+  } else if(i <= 30.0){
+    return("Overweight");
+  } else if (i > 30) {
+    return("Obese");
+  }   
+};
+
